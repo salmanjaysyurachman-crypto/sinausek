b370ddf4089893553957c0603b010dbf3efa9664
Salman <salman@gmail.com>
2026-02-06 10:32:27 +0700

    selesaikan main.py

--- src/main.py
+++ src/main.py
@@ -11,3 +11,37 @@ def menu():
     print("4. Kembalikan Buku")
     print("5. Buku Terpopuler")
     print("0. Keluar")
+
+def main():
+    # Setup database & tabel (aman dipanggil berkali-kali)
+    setup_database()
+
+    while True:
+        menu()
+        pilihan = input("Pilih menu: ")
+
+        if pilihan == "1":
+            tampilkan_buku()
+
+        elif pilihan == "2":
+            tambah_buku()
+
+        elif pilihan == "3":
+            pinjam_buku()
+
+        elif pilihan == "4":
+            kembalikan_buku()
+
+        elif pilihan == "5":
+            buku_terpopuler()
+
+        elif pilihan == "0":
+            print("Terima kasih, program selesai.")
+            break
+
+        else:
+            print("❌ Pilihan tidak valid, silakan coba lagi.")
+
+
+if __name__ == "__main__":
+    main()
