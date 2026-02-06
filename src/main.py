96d9d3ec7432d803218531d73364ca293caf4ab4
Salman <salman@gmail.com>
2026-02-06 10:29:24 +0700

    tambah menu di main.py

--- src/main.py
+++ src/main.py
@@ -1,3 +1,13 @@
 from database import setup_database
 from buku import tampilkan_buku, tambah_buku, buku_terpopuler
 from transaksi import pinjam_buku, kembalikan_buku
+
+
+def menu():
+    print("\n=== SISTEM INFORMASI PERPUSTAKAAN ===")
+    print("1. Tampilkan Buku")
+    print("2. Tambah Buku")
+    print("3. Pinjam Buku")
+    print("4. Kembalikan Buku")
+    print("5. Buku Terpopuler")
+    print("0. Keluar")
